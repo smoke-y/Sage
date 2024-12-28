d0f5430b92bc57a8adb2679d70d30712a826b52e
Vish <SnowSneeze@protonmail.com>
2024-12-28 20:16:56 +0530

    gemma done

--- sage/gemma.py
+++ sage/gemma.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from siglip import *
 from dataclasses import dataclass
 
 @dataclass
@@ -18,6 +19,15 @@ class GemmaConfig:
     attention_dropout: float = 0.0
     pad_token_id: bool = False
 
+@dataclass
+class PaliGemmaConfig:
+    projection_dim: int = 2048
+    hidden_size: int = 2048
+    image_token_index: int
+    pad_token_id: int
+    vision_config: SiglipConfig
+    text_config: GemmaConfig
+
 def rotate_half(x):
     # Build the [-x2, x1, -x4, x3, ...] tensor for the sin part of the positional encoding.
     x1 = x[..., : x.shape[-1] // 2] # Takes the first half of the last dimension
@@ -35,6 +45,9 @@ class KVCache:
     def __init__(self) -> None:
         self.key_cache = []
         self.val_cache = []
+    def num_items(self) -> int:
+        if len(self.key_cache) == 0: return 0
+        return self.key_cache[0].shape[-2]  #[batchSize, numHead, seqLen, headDim]
     def update(self, key: torch.Tensor, val: torch.Tensor, layerId: int):
         if len(self.key_cache) < layerId:
             self.key_cache.append(key)
@@ -124,7 +137,66 @@ class GemmaModel(nn.Module):
         self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, config.pad_token_id)
         self.layers = nn.ModuleList([GemmaDecoderLayer(device) for _ in range(config.num_hidden_layers)])
         self.norm = GemmaRMSNorm(config.hidden_size, config.rms_norm_eps)
-    def forward(self, x: torch.Tensor, attention_mask, position_ids, input_emb, kv_cache) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, attention_mask, position_ids, kv_cache) -> torch.Tensor:
         x = x * self.norm
         for decode_layer in self.layers: x = decode_layer(x, attention_mask, position_ids, kv_cache)
         return self.norm(x)
+class GemmaForCausalLM(nn.Module):
+    def __init__(self, device, config: GemmaConfig) -> None:
+        super().__init__()
+        self.model = GemmaModel(device, config)
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+    def forward(self, x: torch.Tensor, attention_mask, position_ids, kv_cache) -> torch.Tensor:
+        output = self.model.forward(x, attention_mask, position_ids, kv_cache)
+        return self.lm_head(output)
+class PaliGemmaMultiModalProjector(nn.Module):
+    def __init__(self, config: PaliGemmaConfig) -> None:
+        super().__init__()
+        self.linear = nn.Linear(config.vision_config.hidden_size, config.projection_dim)
+    def forward(self, x: torch.Tensor) -> torch.Tensor: return self.linear(x)
+class PaliGemmaForConditionalGeneration(nn.Module):
+    def __init__(self, config: PaliGemmaConfig) -> None:
+        super().__init__()
+        self.scale = config.hidden_size ** 0.5
+        self.image_token_id = config.image_token_index
+        self.pad_token_id = config.pad_token_id
+        self.vision_tower = SiglipVisionTransformer(config.vision_config)
+        self.multi_modal_projector = PaliGemmaMultiModalProjector(config)
+        self.language_model = GemmaForCausalLM(config.text_config)
+    def tie_weights(self) -> None: self.language_model.lm_head.weight = self.language_model.model.embed_tokens.weight
+    def _merge_input_ids_with_image_feature(self, image_feature: torch.Tensor, input_id: torch.Tensor, input_emb: torch.Tensor, attention_mask: torch.Tensor, kv_cache = None):
+        _, _, emb_dim = image_feature.shape
+        batch_size, seq_len = input_id.shape
+        image_feature = image_feature / self.scale
+        final_emb = torch.zeros(batch_size, seq_len, emb_dim, dtype=input_emb.dtype, device=input_emb.device)
+        text_mask = (input_id != self.image_token_id) & (input_id != self.pad_token_id)
+        image_mask = input_id == self.image_token_id
+        pad_mask = input_id == self.pad_token_id
+        text_mask = text_mask.unsqueeze(-1).expand(-1, -1, emb_dim)
+        image_mask = image_mask.unsqueeze(-1).expand(-1, -1, emb_dim)
+        pad_mask = pad_mask.unsqueeze(-1).expand(-1, -1, emb_dim)
+        final_emb = torch.where(text_mask, input_emb, final_emb)
+        final_emb = final_emb.masked_scatter(image_mask, image_feature)
+        final_emb = torch.where(pad_mask, torch.zeros_like(final_emb), final_emb)
+
+        q_len = input_emb.shape[1]  #(batchSize, seqLen, embDim)
+        device = input_emb.device
+        #causal mask has to be of dim (batchSize, numHeads, seqLenQ, seqLenKV)
+        if kv_cache is None:
+            causal_mask = torch.full((batch_size, q_len, q_len), fill_value=0, dtype=input_emb.dtype, device=device)
+            causal_mask = causal_mask.unsqueeze(1)
+            position_ids = (attention_mask.cumsum(-1)).masked_fill_((attention_mask == 0), 1).to(device)
+        else:
+            assert q_len == 1
+            kv_len = kv_cache.num_items() + q_len
+            causal_mask = torch.full((batch_size, q_len, kv_len), fill_value=0, dtype=input_emb.dtype, device=device)
+            causal_mask = causal_mask.unsqueeze(1)
+            position_ids = attention_mask.cumsum(-1)[:, -1]
+            if position_ids.dim() == 1: position_ids = position_ids.unsqueeze(0)
+        return final_emb, causal_mask, position_ids
+    def forward(self, input_ids, pixel_values, attention_mask, kv_cache):
+        input_emb = self.language_model.model.embed_tokens(input_ids)
+        image_feat = self.vision_tower.forward(pixel_values.to(input_emb.dtype))
+        image_feat = self.multi_modal_projector.forward(image_feat)
+        input_emb, attention_mask, position_id = self._merge_input_ids_with_image_feature(image_feat, input_ids, input_emb, attention_mask, kv_cache)
+        return self.language_model.forward(input_emb, attention_mask, position_id, kv_cache)
