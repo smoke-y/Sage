d84473f27b0152f8339d0b581ce2b3ea05918517
Vish <SnowSneeze@protonmail.com>
2025-02-08 15:34:51 +0530

    lora

--- sage/model.py
+++ sage/model.py
@@ -1,12 +1,20 @@
 import copy
 import torch
 import torch.nn as nn
-import loralib as lora
+import torch.optim.optimizer
 from language_model.qwen import *
 from vision_model.siglip import *
 from transformers.utils import logging
 
 logging.set_verbosity_error()
+
+class LoRa(nn.Module):
+    def __init__(self, featureIn: int, featureOut: int, rank: int, alpha: float) -> None:
+        super().__init__() 
+        self.lora_a = nn.Parameter(torch.zeros(rank, featureOut))
+        self.lora_b = nn.Parameter(torch.zeros(featureIn, rank))
+        self.scale = alpha / rank
+    def forward(self, originalWeight: torch.Tensor) -> torch.Tensor: return originalWeight + (self.lora_b @ self.lora_a)*self.scale
 
 class SageConfig:
     def __init__(self, vision_config: SiglipVisionConfig, text_config: Qwen2Config) -> None:
@@ -38,11 +46,13 @@ class Sage(nn.Module):
         self.vision_tower = SiglipVisionModel(config.vision_config)
         self.multi_modal_projector = SageMultiModalProjector(config)
     @staticmethod
-    def from_pretrained(device: torch.device):
+    def from_pretrained(device: torch.device, lora:bool):
         from transformers import LlavaForConditionalGeneration
         pre_model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-interleave-qwen-0.5b-hf")
         config = SageConfig(copy.deepcopy(pre_model.config.vision_config), copy.deepcopy(pre_model.config.text_config))
-        model = Sage(config).to(device)
+        model = Sage(config)
+        if lora: model.applyLoRa()
+        model = model.to(device)
         for name, param in pre_model.named_parameters():
             if name in model.state_dict(): model.state_dict()[name].copy_(param)
         del pre_model
@@ -54,19 +64,56 @@ class Sage(nn.Module):
             inputs_embeds=input_emb
         )
         return logits
+    def applyLoRa(self, rank: int = 4, alpha: float = 32.0):
+        def applyLoraToLayer(layer):
+            nonlocal NonLoRaParam, LoRaParam
+            if hasattr(layer, "weight"):
+                layer.requires_grad = False
+                NonLoRaParam += layer.weight.nelement()
+                if hasattr(layer, "bias"):
+                    if layer.bias is not None: NonLoRaParam += layer.bias.nelement()
+                if len(layer.weight.shape) == 1: return
+                torch.nn.utils.parametrize.register_parametrization(
+                    layer, "weight", LoRa(*layer.weight.shape, rank, alpha)
+                )
+                loraParam = layer.parametrizations["weight"][0]
+                LoRaParam += loraParam.lora_a.nelement() + loraParam.lora_b.nelement()
+        def recurseAndApply(module):
+            for name, child in module.named_children(): recurseAndApply(child)
+            if hasattr(module, "weight") and type(module) in [nn.Linear, nn.Embedding]: applyLoraToLayer(module)
+        with torch.no_grad():
+            NonLoRaParam = 0
+            LoRaParam = 0
+            recurseAndApply(self)
+            print(
+                f"Original param count: {NonLoRaParam}\n"
+                f"LoRa: {LoRaParam}\n"
+                f"Total: {LoRaParam + NonLoRaParam}\n"
+                f"Increment: {(LoRaParam / NonLoRaParam) * 100:.2f}%"
+            )
+    def saveLoRaWeights(self, fileName: str):
+        loraWeights = {}
+        for name, layer in self.named_parameters():
+            if name.endswith("lora_a") or name.endswith("lora_b"): loraWeights[name] = layer.detach().cpu()
+        torch.save(loraWeights, fileName)
+        print("LoRa weights saved to", fileName)
+    def loadLoRaWeights(self, fileName: str):
+        loraWeights = torch.load(fileName, weights_only=True)
+        for name, layer in self.named_parameters():
+            if name.endswith("lora_a") or name.endswith("lora_b"): layer.data.copy_(loraWeights[name])
+        print("LoRa weights loaded from", fileName)
     
 import numpy as np
 from PIL import Image
 
 class Pipeline:
-    def __init__(self, device: torch.device, train: bool = False) -> None:
+    def __init__(self, device: torch.device, lora:bool=False) -> None:
         print("Using", device)
         torch.set_float32_matmul_precision("high")
         from transformers import AutoTokenizer
         self.tokenizer = AutoTokenizer.from_pretrained("llava-hf/llava-interleave-qwen-0.5b-hf")
-        self.model = Sage.from_pretrained(device)
+        self.model = Sage.from_pretrained(device, lora)
         self.device = device
-        if train: lora.mark_only_lora_as_trainable(self.model)
     def first_emb(self, text: str, img: Image) -> torch.Tensor:
         size = self.model.config.vision_config.image_size
         img = img.resize((size, size))
@@ -81,9 +128,14 @@ class Pipeline:
         image_emb = self.model.multi_modal_projector(image)
         index_to_replace = (input_ids == self.model.config.image_token_index).nonzero(as_tuple=True)[0].item()
         return torch.cat([input_emb[:, :index_to_replace, :], image_emb, input_emb[:, index_to_replace+1: , :]], dim=1).to(self.device)
+    def get_optimizer(self):
+        param = []
+        for name, parameter in self.model.named_parameters():
+            if name.endswith("lora_a") or name.endswith("lora_b"): param.append(parameter)
+        return torch.optim.Adam([{"params": param}], lr=0.01, fused=True)
     def get_logits(self, input_emb: torch.Tensor) -> torch.Tensor:
         with torch.autocast(device_type=self.device, dtype=torch.bfloat16): return self.model.forward(input_emb)
-    def generate(self, input_emb: torch.Tensor, labels: torch.Tensor = None, temp: float = 0.7, top_k: float = 50) -> torch.Tensor:
+    def generate(self, input_emb: torch.Tensor, labels: torch.Tensor = None, temp: float = 0.7, top_k: float = 50):
         logits = self.get_logits(input_emb)
         top_k_logits, top_k_indices = torch.topk(logits[:, -1, :], top_k)
         probs = torch.softmax(top_k_logits / temp, dim=-1)
@@ -92,7 +144,6 @@ class Pipeline:
         if labels is not None:
             logits = logits[:, -1, :]
             loss = nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), labels.view(-1))
-        return top_k_indices.gather(-1, pred), loss
+        b = top_k_indices.gather(-1, pred)
+        return b, loss
     def get_emb(self, index: torch.Tensor) -> torch.Tensor: return self.model.language_model.get_input_embeddings()(index)
-    def save_lora(self, path: str) -> None: torch.save(lora.lora_state_dict(self.model), path)
-    def load_lora(self, path: str) -> None: self.model.load_state_dict(torch.load(path), strict=False)
--- sage/train.py
+++ sage/train.py
@@ -4,13 +4,27 @@ from datasets import load_dataset
 
 ds = load_dataset("flaviagiammarino/vqa-rad")
 device = "cuda" if torch.cuda.is_available() else "cpu"
-pipe = Pipeline(device, train=True)
+pipe = Pipeline(device, lora=True)
+optim = pipe.get_optimizer()
 stop_token = pipe.model.config.text_config.eos_token_id
 train = ds["train"].shuffle()
-for i in train:
-    input_emb = pipe.first_emb(i["question"], i["image"])
-    lenQues = input_emb.shape[1]
-    ans_id = torch.tensor(pipe.tokenizer.encode(i["answer"]) + [stop_token]).unsqueeze(0).to(device)
-    for j in range(ans_id[:, 1]):
-        predicted_token_index, loss = pipe.generate(input_emb, ans_id[:, j])
-        input_emb = torch.cat([input_emb, pipe.get_emb(predicted_token_index)], dim=1)
+x = 0
+log = open("trace.log", "w+")
+for epoch in range(10):
+    for i in train:
+        optim.zero_grad()
+        input_emb = pipe.first_emb(i["question"], i["image"])
+        lenQues = input_emb.shape[1]
+        ans_id = torch.tensor(pipe.tokenizer.encode(i["answer"]) + [stop_token]).unsqueeze(0).to(device)
+        for j in range(ans_id.shape[1]):
+            predicted_token_index, loss = pipe.generate(input_emb, ans_id[:, j])
+            input_emb = torch.cat([input_emb, pipe.get_emb(ans_id[:, j]).unsqueeze(0)], dim=1)
+            log.write(f"{loss.detach().cpu().numpy()}\n")
+            loss.backward()
+            optim.step()
+        if x == 7:
+            x = 0
+            pipe.model.saveLoRaWeights("lora.pth")
+        x += 1
+pipe.model.saveLoRaWeights("lora.pth")
+log.close()
